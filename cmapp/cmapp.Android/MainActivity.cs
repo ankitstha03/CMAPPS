5072bfb5249490dd87e3ee9c442d965ad5c9e3b3
ankitstha03 <071bct505@pcampus.edu.np>
2018-02-17 11:03:50 +0545

    icon and name

--- cmapp/cmapp.Android/MainActivity.cs
+++ cmapp/cmapp.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Firebase.Iid;
 
 namespace cmapp.Droid
 {
-    [Activity(Label = "cmapp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Shankar Pokhrel", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
