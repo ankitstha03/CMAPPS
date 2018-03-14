c1e71e83354a80793912190361aac3cb9708184c
ankitstha03 <34916738+ankitstha03@users.noreply.github.com>
2018-03-14 21:25:36 +0545

    Update News.cs

--- cmapp/cmapp/Models/News.cs
+++ cmapp/cmapp/Models/News.cs
@@ -8,7 +8,6 @@ namespace cmapp.Models
     {
         public string title { get; set; }
         public string description { get; set; }
-        public string desc { get; set; }
         public string url { get; set; }
         public string images { get; set; }
     }
