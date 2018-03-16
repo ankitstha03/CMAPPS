f0b1936e22c718956a48edefad8506f3395258c6
bikram101 <bekrumed1@gmail.com>
2018-03-16 10:29:50 +0545

    Add files via upload

---
+++ cmapp/cmapp/TemplateViews/VideoTemplate.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using cmapp.Models;
+namespace cmapp.TemplateViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VideoTemplate : ContentView
+    {
+        public VideoTemplate()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
